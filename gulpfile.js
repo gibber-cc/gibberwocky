ca57bb85d8efb1c19f9f9735e481b14c5d08d287
charlieroberts <charlie@charlie-roberts.com>
2015-12-17 14:26:11 -0500

    added watchify to gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var transform = require('vinyl-transform');
 var source = require('vinyl-source-stream');
+var watchify = require( 'watchify' )
+var gutil = require( 'gulp-util' )
 
-gulp.task('default', function () {
+gulp.task('build', function () {
   
   var b = browserify({
     entries: './js/index.js',
@@ -12,3 +14,17 @@ gulp.task('default', function () {
 
   b.pipe( source('index.js') ).pipe( gulp.dest( './dist/' ) )
 });
+
+
+watchify.args.entries = './js/index.js'
+watchify.args.debug = true
+
+var b = watchify( browserify( watchify.args ) )
+b.on( 'update', bundle )
+b.on( 'log', gutil.log )
+
+gulp.task('default', bundle)
+
+function bundle() {
+  return b.bundle().pipe( source('index.js') ).pipe( gulp.dest( './dist/' ) )
+}
