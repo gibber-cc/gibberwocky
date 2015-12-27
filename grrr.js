9a0b289cfc0ee57561e255107bdebd263e55da9f
Graham Wakefield <grrrwaaa@gmail.com>
2015-12-27 11:53:45 -0500

    updated grrr amxd to use websocket communication

--- grrr.js
+++ grrr.js
@@ -1,9 +1,6 @@
 
 
-var notes = [
-	//60, 
-	62, 64, 65, 67, 69, 71, 72
-];
+var notes = [ 60, 62, 64, 65, 67, 69, 71, 72 ];
 
 function random(n) {
 	return Math.floor(Math.random()*n);
@@ -14,15 +11,10 @@ function pick(arr) {
 }
 
 function seq(seqname) {
-	outlet(0, "delete", seqname, -1, 5.); // clear it from timepoints -1 to 2
+	outlet(0, "delete", seqname, -1, 2.); // clear over all valid timepoints
 	for (var i=0; i< 32; i++) {
 		var div = Math.pow(2, random(3));
 		var t = random(div)/div;
 		outlet(0, "add", seqname, t, pick(notes), 64 + random(32));
 	}
-}
-
-// initialize:
-for (var i=0; i<8; i++) {
-	seq(i);
 }
