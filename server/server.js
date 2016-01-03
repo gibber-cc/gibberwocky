34cd3424e0864766cbf41aea668c5f61ff2c5879
charlieroberts <charlie@charlie-roberts.com>
2016-01-03 14:37:33 -0800

    cleanup server

--- server/server.js
+++ server/server.js
@@ -5,38 +5,19 @@ var ws     = require( 'ws' ),
 		ROOTDIR       = __dirname + '/../client',
 		WEBSERVERPORT = 9080
 
-console.log( "ROOT IS ", root )
+console.log( 'root directory is', ROOTDIR )
+
 var Server = {
   createServers : function() {
-		/*
-     *if this.servers[ port ] ) return this.servers[ port ]
-     *
-     *var server = new ( require( 'ws' ).Server )({ 'port': port })
-     *
-     *server.clients = {} // TODO: this is already an array defined by the ws module.
-     *
-     *server.on( 'connection', this.onClientConnection.bind( server ) )
-     *
-     *server.output = function( path, typetags, values ) { // TODO: you should be able to target individual clients
-     *  for( var i = 0; i < server.clients.length; i++ ) {
-     *    var client = server.clients[ i ]
-     *    client.send( JSON.stringify({ 'key': path, 'values': Array.isArray( values ) ? values : [ values ] }) )
-     *  }
-     *}
-     *
-     *WS.servers[ port ] = server
-     *
-     *this.emit( 'WebSocket server created', server, port )
-		 */
     this.createHTTPServer()
     this.createWebSocketServer()	
   },
 
 	createWebSocketServer: function() {
-	 this.wss = new ws.Server({ server: server })
-	 this.wss.on( 'connection', this.onClientConnection )
-   server.on( 'request', app )
-	 server.listen( WEBSERVERPORT, function() { console.log( 'listening...' ) } )
+	  this.wss = new ws.Server({ server: server })
+	  this.wss.on( 'connection', this.onClientConnection )
+    server.on( 'request', app )
+	  server.listen( WEBSERVERPORT, function() { console.log( 'listening...' ) } )
 	},
 
   createHTTPServer: function() {
@@ -44,22 +25,16 @@ var Server = {
   },
 
   onClientConnection : function( client ) { // "this" is bound to a ws server
-		console.log( 'CONNECTION', client )
-    client.ip = client._socket.remoteAddress;
-    //  this.clients[ client.ip ] = WS.clients[ client.ip ] = client
+    console.log( 'client connection.' )
+		
+		client.ip = client._socket.remoteAddress;
+
     
     client.on( 'message', function( _msg ) {
       var msg = JSON.parse( _msg )
       
 			console.log( "MSG CODE", msg.code )
     })
-    
-		/*
-     *client.on( 'close', function() {
-     *  delete WS.clients[ client.ip ]
-     *})
-		 */
-    
   },
 }
 
