9d723e04b2dd250383a8a0ec7492894c9224d988
charlieroberts <charlie@charlie-roberts.com>
2016-04-08 23:07:34 -0400

    added pickRandomParameter() to device

--- js/live.js
+++ js/live.js
@@ -23,6 +23,18 @@ let Live = {
     for( let device of track.devices ) {
       let d = Gibber.currentTrack.devices[ device.title ] = { idx:deviceCount++ },
           parameterCount = 0
+      
+      d.pickRandomParameter = ()=> {
+        let idx = Gibber.Utility.rndi( 0, device.parameters.length - 1 ),
+            param = device.parameters[ idx ]
+         
+        while( param.name === 'Device On' ) {
+          idx = Gibber.Utility.rndi( 0, device.parameters.length - 1 ),
+          param = device.parameters[ idx ]
+        }
+
+        return d[ param.name ]
+      }
 
       for( let parameter of device.parameters ) {
         let v = parameter.value,
@@ -39,8 +51,7 @@ let Live = {
             if( typeof _v === 'object' && _v.isGen ) {
               _v.assignParamID( parameter.id )
               Gibber.Communication.send( `${Gibber.Live.id} gen ${parameter.id} "${_v.out()}"` )
-              v = _v
-              
+            }else{
               v = _v
               Gibber.Communication.send( `${Gibber.Live.id} set ${parameter.id} ${v}` )
             }
